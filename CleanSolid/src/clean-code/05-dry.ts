703283b4b2786e149be4b75bccabdd6a849087dd
Ricardo Mejias <ricardo@mftech.io>
2022-11-29 16:08:00 -0400

    Aplicando DRY

--- CleanSolid/src/clean-code/05-dry.ts
+++ CleanSolid/src/clean-code/05-dry.ts
@@ -7,18 +7,38 @@ class Product {
     public size: Size = ""
   ) {}
 
+  isProductReady(): boolean {
+    for (const key in this) {
+      switch (typeof this[key]) {
+        case "string":
+          if ((this[key] as string).length <= 0) throw Error(`${key} is empty`);
+          break;
+        case "number":
+          if (this[key] <= 0) throw Error("Price is zero");
+          break;
+        default:
+          throw Error(`${typeof this[key]} is not supperted`);
+      }
+    }
+
+    return true;
+  }
+
   toString() {
     // No Dry
-    if (this.name.length <= 0) throw Error("Name is empty");
-    if (this.price <= 0) throw Error("Price is zero");
-    if (this.size.length <= 0) throw Error("Size is empty");
+    // if (this.name.length <= 0) throw Error("Name is empty");
+    // if (this.price <= 0) throw Error("Price is zero");
+    // if (this.size.length <= 0) throw Error("Size is empty");
+
+    if (!this.isProductReady()) return;
 
     return `${this.name} (${this.price}, ${this.size})`;
   }
 }
 
 (() => {
-  const bluePants = new Product("Blue Large pants", 10, 'M');
+  // "Blue Large pants", 10, "M"
+  const bluePants = new Product("Blue Large pants", 10, "M");
 
   console.log(bluePants.toString());
 })();
