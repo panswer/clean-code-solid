68080601af1fde471b9608781170d79a991c5728
Ricardo Mejias <ricardo@mftech.io>
2022-11-30 11:04:40 -0400

    Herencia - Problematica

--- CleanSolid/src/clean-code/06-classes-a.ts
+++ CleanSolid/src/clean-code/06-classes-a.ts
@@ -1,8 +1,9 @@
 (() => {
+  // No aplicando el principio de responsabilidad unica
   type Gender = "M" | "F";
   /* 
-        Clase declarada de forma larga
-    */
+    Clase declarada de forma larga
+  */
   class Person {
     constructor(
       public name: string,
@@ -11,7 +12,51 @@
     ) {}
   }
 
-  const newPerson = new Person("Ricardo Mejias", "M", new Date("1997-04-09"));
+  class User extends Person {
+    private lastAccess: Date;
 
-  console.log(newPerson);
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+  }
+
+  const userSettings = new UserSettings(
+    "/user/home",
+    "/home",
+    "ricardo@gmail.com",
+    "Admin",
+    "Ricardo Mejias",
+    "M",
+    new Date("1997/04/09")
+  );
+  console.log({
+    userSettings,
+    isChecked: userSettings.checkCredentials(),
+  });
 })();
