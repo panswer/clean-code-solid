0c264035969a0f6d1b8887c3662ef44c32595b05
Ricardo Mejias <ricardo@mftech.io>
2022-12-02 11:48:28 -0400

    Remove la dependencia de Axios

--- CleanSolid/src/solid/03-open-close-c.ts
+++ CleanSolid/src/solid/03-open-close-c.ts
@@ -1,12 +1,22 @@
-import axios from "axios";
+// import axios from "axios";
 
 export class HttpClient {
+  //   async get(url: string) {
+  //     const { data, status } = await axios.get(url);
+
+  //     return {
+  //       data,
+  //       status,
+  //     };
+  //   }
+
   async get(url: string) {
-    const { data, status } = await axios.get(url);
+    const resp = await fetch(url);
+    const data = await resp.json();
 
     return {
       data,
-      status,
+      status: resp.status,
     };
   }
 }
