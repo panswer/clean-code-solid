11978adc731600e06493b3c6240c272c30caf94e
Ricardo Mejias <ricardo@mftech.io>
2022-12-02 09:55:27 -0400

    Ejemplo de SRP - Segunda Parte

--- CleanSolid/src/solid/01-srp.ts
+++ CleanSolid/src/solid/01-srp.ts
@@ -19,38 +19,45 @@
     private email: string = "fernando@google.com";
 
     sendEmail(emailList: string[], template: "to-clients" | "to-admin") {
-      console.log("Enviando correo a los clientes",  template);
+      console.log("Enviando correo a los clientes", template);
     }
   }
 
   // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
   // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
   class ProductBloc {
+    private productService: ProductService;
+    private mailer: Mailer;
+
+    constructor(productService: ProductService, mailer: Mailer) {
+      this.productService = productService;
+      this.mailer = mailer;
+    }
+
     loadProduct(id: number) {
-      // Realiza un proceso para obtener el producto y retornarlo
-      console.log("Producto: ", { id, name: "OLED Tv" });
+      this.productService.getProduct(id);
     }
 
     saveProduct(product: Product) {
-      // Realiza una petición para salvar en base de datos
-      console.log("Guardando en base de datos", product);
+      this.productService.saveProduct(product);
     }
 
     notifyClients() {
-      console.log("Enviando correo a los clientes");
+      this.mailer.sendEmail(["eduardo@google.com"], "to-clients");
     }
   }
 
   class CartBloc {
-    private itemsInCart: Object[] = [];
-
     addToCart(productId: number) {
       // Agregar al carrito de compras
       console.log("Agregando al carrito ", productId);
     }
   }
 
-  const productBloc = new ProductBloc();
+  const productService = new ProductService();
+  const mailer = new Mailer();
+
+  const productBloc = new ProductBloc(productService, mailer);
   const cartBloc = new CartBloc();
 
   productBloc.loadProduct(10);
